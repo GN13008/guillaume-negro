287407efe31e637b6a768e0e947a06e9b8a391b8
Guillaume Negro <negro.guillaume@gmail.com>
2021-09-13 15:07:08 +0200

    some work on the navbar

--- main.js
+++ main.js
@@ -2,3 +2,56 @@ const btn = document.querySelector("#clickme");
 btn.addEventListener("click", (event) => {
   console.log("hello");
 });
+
+const maNavBar = document.querySelector('.navbar');
+const maBanner = document.querySelector('.banner');
+
+
+// copy from my WesBos course
+
+function debounce(func, wait = 10, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+// const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  if (window.scrollY > (maBanner.scrollHeight / 5)) {
+    maNavBar.classList.remove("transparentnav")
+    maNavBar.classList.add("whitenav")
+  } else {
+    maNavBar.classList.add("transparentnav")
+    maNavBar.classList.remove("whitenav")
+  }
+  // const isScrolledPast = slideInAt > sliderImage.offsetTop;
+  // const isNotScrolledPast = window.scrollY < imageBottom;
+
+
+
+  // sliderImages.forEach(sliderImage => {
+  //   // half way through the image
+  //   const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+  //   // bottom of the image
+  //   const imageBottom = sliderImage.offsetTop + sliderImage.height;
+  //   const isHalfShown = slideInAt > sliderImage.offsetTop;
+  //   const isNotScrolledPast = window.scrollY < imageBottom;
+  //   if (isHalfShown && isNotScrolledPast) {
+  //     sliderImage.classList.add('active');
+  //   } else {
+  //     sliderImage.classList.remove('active');
+  //   }
+  // })
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
